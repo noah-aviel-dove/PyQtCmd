11b220869e7215871c7feb9994ff3e0521bf0a8a
Noah Aviel Dove <nadove@ucsc.edu>
2023-03-12 03:03:41 -0700

    Clean up copied code

--- copy.py
+++ copy.py
@@ -6,233 +6,186 @@ in a GUI application.
 
 Original by deanhystad available here: https://python-forum.io/thread-25117.html
 """
-
-import sys
 import code
+import collections
+import contextlib
 import re
-from typing import Callable
+import sys
+import typing
+
 from PyQt5 import (
     QtCore,
     QtGui,
     QtWidgets,
 )
-from contextlib import (
-    redirect_stdout,
-    redirect_stderr,
-)
+
+_default_max_history = 100
+_default_max_line_continuations = 500
 
 
-class LineEdit(QtWidgets.QLineEdit):
+class QCmdLineEdit(QtWidgets.QLineEdit):
     """
     QLineEdit with a history buffer for recalling previous lines.
-    I also accept tab as input (4 spaces).
+    The `returnPressed` signal is not used.
+    Switching widget focus via the tab key is disabled so that `\t` characters
+    can be entered as part of the text.
     """
-    newline = QtCore.pyqtSignal(str)  # Signal when return key pressed
+    line_entered = QtCore.pyqtSignal(str)
 
-    def __init__(self, history: int = 100):
+    def __init__(self,
+                 *,
+                 max_history: int = _default_max_history
+                 ):
         super().__init__()
-        self.historymax = history
-        self.clearhistory()
-        self.promptpattern = re.compile('^[>\.]')
+        self.history = collections.deque(maxlen=max_history)
+        self.history_index = 0
+        self.history.appendleft('')
 
-    def clearhistory(self) -> None:
-        """
-        Clear history buffer
-        """
-        self.historyindex = 0
-        self.historylist = []
+    def event(self, a0: QtCore.QEvent) -> bool:
+        if a0.type() == QtCore.QEvent.Type.KeyPress and a0.key() == QtCore.Qt.Key_Tab:
+            self.insert('\t')
+            return True
+        else:
+            return super().event(a0)
 
-    def event(self, ev: QtCore.QEvent) -> bool:
-        """
-        Intercept tab and arrow key presses. Insert 4 spaces when tab pressed
-        instead of moving to next control. When arrow up or down are pressed,
-        select a line from the history buffer. Emit newline signal when return
-        key is pressed.
-        """
-        if ev.type() == QtCore.QEvent.KeyPress:
-            if ev.key() == int(QtCore.Qt.Key_Tab):
-                self.insert('    ')
-                return True
-            elif ev.key() == int(QtCore.Qt.Key_Up):
-                self.recall(self.historyindex - 1)
-                return True
-            elif ev.key() == int(QtCore.Qt.Key_Down):
-                self.recall(self.historyindex + 1)
-                return True
-            elif ev.key() == int(QtCore.Qt.Key_Home):
-                self.recall(0)
-                return True
-            elif ev.key() == int(QtCore.Qt.Key_End):
-                self.recall(len(self.historylist) - 1)
-                return True
-            elif ev.key() == int(QtCore.Qt.Key_Return):
-                self.returnkey()
-                return True
-        return super().event(ev)
+    def keyPressEvent(self, a0: QtGui.QKeyEvent) -> None:
+        if a0.key() == int(QtCore.Qt.Key_Up):
+            self._prev()
+        elif a0.key() == int(QtCore.Qt.Key_Down):
+            self._next()
+        elif a0.key() == int(QtCore.Qt.Key_Return):
+            self._enter_line()
+        else:
+            super().keyPressEvent(a0)
 
-    def returnkey(self) -> None:
-        """
-        Return key was pressed. Add line to history and emit the newline signal.
-        """
-        text = self.text().rstrip()
-        self.record(text)
-        self.newline.emit(text)
-        self.setText('')
+    def _enter_line(self) -> None:
+        text = self.text()
+        self.history_index = 0
+        self.history[0] = text
+        self.line_entered.emit(text)
+        self.history.appendleft('')
+        self._update()
 
-    def recall(self, index: int) -> None:
-        """
-        Select a line from the history list
-        """
-        length = len(self.historylist)
-        if length > 0:
-            index = max(0, min(index, length - 1))
-            self.setText(self.historylist[index])
-            self.historyindex = index
+    def _prev(self) -> None:
+        if self.history_index < len(self.history) - 1:
+            self.history_index += 1
+            self._update()
 
-    def record(self, line: str) -> None:
-        """
-        Add line to history buffer
-        """
-        self.historyindex += 1
-        while len(self.historylist) >= self.historymax - 1:
-            self.historylist.pop()
-        self.historylist.append(line)
-        self.historyindex = min(self.historyindex, len(self.historylist))
+    def _next(self) -> None:
+        if self.history_index > 0:
+            self.history_index -= 1
+            self._update()
+
+    def _update(self) -> None:
+        self.setText(self.history[self.history_index])
 
 
-class Redirect:
-    """
-    Map `self.write` to a function
-    """
-
-    def __init__(self, func: Callable):
-        self.func = func
-
-    def write(self, line: str) -> None:
-        self.func(line)
-
-
-class Console(QtWidgets.QWidget):
-    """
-    A GUI version of `code.InteractiveConsole`.
-    """
+class QCmdConsole(QtWidgets.QWidget):
 
     def __init__(
         self,
-        context=locals(),  # context for interpreter
-        history: int = 20,  # max lines in history buffer
-        blockcount: int = 500  # max lines in output buffer
+        interpreter: typing.Callable[[str], bool],
+        *,
+        prompt_text: str = '> ',
+        line_continuing_prompt_text: str = '… ',
+        max_history: int = _default_max_history,
+        max_line_continuations: int = _default_max_line_continuations,
+        stdout_foreground: QtGui.QBrush = QtGui.QBrush(QtGui.QColorConstants.Blue),
+        stderr_foreground: QtGui.QBrush = QtGui.QBrush(QtGui.QColorConstants.Red),
+        parent=None
     ):
-        super().__init__()
-        self.setcontext(context)
+        super().__init__(parent=parent)
+        self.interpreter = interpreter
         self.buffer = []
-
-        self.content = QtWidgets.QGridLayout(self)
-        self.content.setContentsMargins(0, 0, 0, 0)
-        self.content.setSpacing(0)
+        self.prompt_text = prompt_text
+        self.line_continuing_prompt_text = line_continuing_prompt_text
+        self._prompt_re = re.compile(f'^{re.escape(self.prompt_text)}|{re.escape(self.line_continuing_prompt_text)}')
 
         # Display for output and stderr
-        self.outdisplay = QtWidgets.QPlainTextEdit(self)
-        self.outdisplay.setMaximumBlockCount(blockcount)
-        self.outdisplay.setReadOnly(True)
-        self.content.addWidget(self.outdisplay, 0, 0, 1, 2)
+        self.display = QtWidgets.QPlainTextEdit(self)
+        # FIXME I think we can just remove this variable
+        self.display.setMaximumBlockCount(max_line_continuations)
+        self.display.setReadOnly(True)
 
         # Use color to differentiate input, output and stderr
-        self.inpfmt = self.outdisplay.currentCharFormat()
-        self.outfmt = QtGui.QTextCharFormat(self.inpfmt)
-        self.outfmt.setForeground(QtGui.QBrush(QtGui.QColor(0, 0, 255)))
-        self.errfmt = QtGui.QTextCharFormat(self.inpfmt)
-        self.errfmt.setForeground(QtGui.QBrush(QtGui.QColor(255, 0, 0)))
+        self.input_format = self.display.currentCharFormat()
 
-        # Display input prompt left of input edit
-        self.promptdisp = QtWidgets.QLineEdit(self)
-        self.promptdisp.setReadOnly(True)
-        self.promptdisp.setFixedWidth(15)
-        self.promptdisp.setFrame(False)
-        self.content.addWidget(self.promptdisp, 1, 0)
-        self.setprompt('> ')
+        self.output_format = QtGui.QTextCharFormat(self.input_format)
+        self.output_format.setForeground(stdout_foreground)
+
+        self.error_format = QtGui.QTextCharFormat(self.input_format)
+        self.error_format.setForeground(stderr_foreground)
+
+        # Display input prompt left of input edit.
+        # A QLabel would be more appropriate, but inconveniently, the default
+        # background colors are inconsistent
+        # FIXME need to go back to QLAbel and adjust palette accordingly
+        #  because lineedit is far too wide without setting a fixed size
+        self.prompt = QtWidgets.QLineEdit(self)
+        self.prompt.setText(self.prompt_text)
+        self.prompt.setReadOnly(True)
+        self.prompt.setFrame(False)
+        self.prompt.setFixedWidth(20)
 
         # Enter commands here
-        self.inpedit = LineEdit(history=history)
-        self.inpedit.newline.connect(self.push)
-        self.inpedit.setFrame(False)
-        self.content.addWidget(self.inpedit, 1, 1)
+        self.line_edit = QCmdLineEdit(max_history=max_history)
+        self.line_edit.line_entered.connect(self._push)
+        self.line_edit.setFrame(False)
 
-    def setcontext(self, context):
-        """
-        Set context for interpreter
-        """
-        self.interp = code.InteractiveInterpreter(context)
+        # FIXME remove empty space between input and history
+        #  currently one is stuck on the bottom and the other on the top;
+        #  textedit probably has a more aggressive size policy than lineedit
+        layout = QtWidgets.QGridLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        layout.addWidget(self.display, 0, 0, 1, 2)
+        layout.addWidget(self.prompt, 1, 0)
+        layout.addWidget(self.line_edit, 1, 1)
+        self.setLayout(layout)
 
-    def resetbuffer(self) -> None:
-        """
-        Reset the input buffer.
-        """
-        self.buffer = []
+        class _StdIn:
+            def write(self_, s: str) -> None:
+                self._write(s, self.input_format)
 
-    def setprompt(self, text: str):
-        self.prompt = text
-        self.promptdisp.setText(text)
+        class _StdOut:
+            def write(self_, s: str) -> None:
+                self._write(s, self.output_format)
 
-    def push(self, line: str) -> None:
+        class _StdErr:
+            def write(self_, s: str) -> None:
+                self._write(s, self.error_format)
+
+        self.stdin = _StdIn()
+        self.stdout = _StdOut()
+        self.stderr = _StdErr()
+
+    def _push(self, line: str) -> None:
         """
         Execute entered command. Command may span multiple lines
         """
-        if line == 'clear':
-            self.inpedit.clearhistory()
-            self.outdisplay.clear()
+        self.stdin.write(self.prompt.text() + line)
+        self.buffer.append(line)
+
+        cmd = '\n'.join(self.buffer)
+        more = self.interpreter(cmd)
+        if more:
+            self.prompt.setText(self.line_continuing_prompt_text)
         else:
-            lines = line.split('\n')
-            for line in lines:
-                if re.match('^[\>\.] ', line):
-                    line = line[2:]
-                self.writeoutput(self.prompt + line, self.inpfmt)
-                self.setprompt('. ')
-                self.buffer.append(line)
-            # Built a command string from lines in the buffer
-            source = "\n".join(self.buffer)
-            more = self.interp.runsource(source, '<console>')
-            if not more:
-                self.setprompt('> ')
-                self.resetbuffer()
+            self.prompt.setText(self.prompt_text)
+            self.buffer.clear()
 
-    def setfont(self, font: QtGui.QFont) -> None:
-        """
-        Set font for input and display widgets. Should be monospaced
-        """
-        self.outdisplay.setFont(font)
-        self.inpedit.setFont(font)
-
-    def write(self, line: str) -> None:
-        """
-        Capture stdout and display in outdisplay
-        """
-        if len(line) != 1 or ord(line[0]) != 10:
-            self.writeoutput(line.rstrip(), self.outfmt)
-
-    def errorwrite(self, line: str) -> None:
-        """
-        Capture stderr and display in `outdisplay`
-        """
-        self.writeoutput(line, self.errfmt)
-
-    def writeoutput(self, line: str, fmt: QtGui.QTextCharFormat = None) -> None:
-        """
-        Set text formatting and display line in `outdisplay`
-        """
+    def _write(self, line: str, fmt: QtGui.QTextCharFormat = None) -> None:
         if fmt is not None:
-            self.outdisplay.setCurrentCharFormat(fmt)
-        self.outdisplay.appendPlainText(line.rstrip())
+            self.display.setCurrentCharFormat(fmt)
+        self.display.appendPlainText(line.rstrip())
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    console = Console()
+    console = QCmdConsole(interpreter=code.InteractiveConsole().runsource)
     console.setWindowTitle('Console')
-    console.setfont(QtGui.QFont('Lucida Sans Typewriter', 10))
+    console.setFont(QtGui.QFont('Lucida Sans Typewriter', 10))
 
-    # Redirect stdout to console.write and stderr to `console.errorwrite`
-    redirect = Redirect(console.errorwrite)
-    with redirect_stdout(console), redirect_stderr(redirect):
+    with contextlib.redirect_stdout(console.stdout), contextlib.redirect_stderr(console.stderr):
         console.show()
         sys.exit(app.exec_())
