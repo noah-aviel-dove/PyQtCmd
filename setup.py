ea338aab3310001b5ec9da9b967ad44dd4ea0338
Noah Aviel Dove <nadove@ucsc.edu>
2023-03-11 21:11:36 -0800

    Initialize project

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='PyQtCmd',
+    version='0.1',
+    description='Command-line interface widget for PyQt5',
+    url='https://github.com/noah-aviel-dove/PyQtCmd',
+    author_email='noah.aviel.dove@gmail.com',
+    license='GPLv3',
+    packages=['PyQtCmd'],
+    python_requires='>=3.11.1',
+    install_requires=['PyQt5']
+)
